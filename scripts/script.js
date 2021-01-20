f1a1365b6ba27f724be7d2cd2e6389536b371dda
RyanBrewer317 <ryanbrew317@gmail.com>
2021-01-19 20:51:53 -0800

    implement memory of the players in the world take 4

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@ var server = express()
 var wss = new Server({ server: server });
 functions.generateNewRegion();
 wss.on('connection', function (socket, req) {
-    var hero = new setup.Hero(socket, req.connection.remoteAddress);
+    var hero = new setup.Hero(socket, req.headers.host);
     var existing = false;
     for (var i = 0; i < setup.Heroes.length; i++) {
         if (setup.Heroes[i].name === hero.name) {
